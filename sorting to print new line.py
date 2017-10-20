ced9726c34272141f0a68fda7d71fe63a0e0af8e
sathish142 <30345031+sathish142@users.noreply.github.com>
2017-10-20 21:11:46 +0530

    Update sorting to print new line.py

--- sorting to print new line.py
+++ sorting to print new line.py
@@ -5,7 +5,7 @@ for i in range(0,n):
     a.append(b)
 for i in range(0,n):
     for j in range(i+1,n):
-        if(a[i]>a[j]):
+        if(a[i]<a[j]):
             t=a[j]
             a[j]=a[i]
             a[i]=t
