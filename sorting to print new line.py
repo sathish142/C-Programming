7e696135c12c405104c1fd4c47354c2ba555c4ec
sathish142 <30345031+sathish142@users.noreply.github.com>
2017-10-20 21:09:10 +0530

    Create sorting to print new line.py

---
+++ sorting to print new line.py
@@ -0,0 +1,13 @@
+a=[]
+n=int(input())
+for i in range(0,n):
+    b=input()
+    a.append(b)
+for i in range(0,n):
+    for j in range(i+1,n):
+        if(a[i]>a[j]):
+            t=a[j]
+            a[j]=a[i]
+            a[i]=t
+for i in range(0,n):
+    print (a[i])
